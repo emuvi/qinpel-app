c0bc695d2d7330ff62244e3ffd7f6142b8354155
Éverton M. Vieira <everton.muvi@gmail.com>
2022-03-15 19:14:22 -0300

    prototype development

--- src/qin-manager.ts
+++ src/qin-manager.ts
@@ -221,26 +221,26 @@ export class QinManager {
 }
 
 function setCookie(name: any, value: any, options: any = {}) {
+  options = {
+    path: "/",
+    // add other defaults here if necessary
+    ...options,
+  };
 
-    options = {
-      path: '/',
-      // add other defaults here if necessary
-      ...options
-    };
-  
-    if (options.expires instanceof Date) {
-      options.expires = options.expires.toUTCString();
-    }
-  
-    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
-  
-    for (let optionKey in options) {
-      updatedCookie += "; " + optionKey;
-      let optionValue = options[optionKey];
-      if (optionValue !== true) {
-        updatedCookie += "=" + optionValue;
-      }
-    }
-  
-    document.cookie = updatedCookie;
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString();
   }
+
+  let updatedCookie =
+    encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
