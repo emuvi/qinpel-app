a8a5127f6b0766e77478740a8205acf839213b40
Éverton M. Vieira <emuvi@goutlook.com.br>
2022-05-16 17:56:37 -0300

    adds listeners to popup events

--- src/qin-jobber-popup.ts
+++ src/qin-jobber-popup.ts
@@ -11,6 +11,9 @@ export class QinJobberPopup {
   private _maxWidth: number;
   private _maxHeight: number;
 
+  private _onShow: Array<Function> = null;
+  private _onClose: Array<Function> = null;
+
   public constructor(jobber: QinJobber, divContent: HTMLDivElement) {
     this._jobber = jobber;
     this._divContent = divContent;
@@ -46,6 +49,38 @@ export class QinJobberPopup {
     return QinSoul.arm.stopEvent(ev);
   }
 
+  public addOnShow(func: Function) {
+    if (!this._onShow) {
+      this._onShow = [];
+    }
+    this._onShow.push(func);
+  }
+
+  public delOnShow(func: Function) {
+    if (this._onShow) {
+      let index = this._onShow.indexOf(func);
+      if (index > -1) {
+        this._onShow.splice(index, 1);
+      }
+    }
+  }
+
+  public addOnClose(func: Function) {
+    if (!this._onClose) {
+      this._onClose = [];
+    }
+    this._onClose.push(func);
+  }
+
+  public delOnClose(func: Function) {
+    if (this._onClose) {
+      let index = this._onClose.indexOf(func);
+      if (index > -1) {
+        this._onClose.splice(index, 1);
+      }
+    }
+  }
+
   public show() {
     this.close();
     const iframeBody = this._jobber.getIFrameDoc().body;
@@ -58,12 +93,12 @@ export class QinJobberPopup {
     this._divMain.style.top = this._posY + "px";
     this._divMain.style.maxWidth = this._maxWidth + "px";
     this._divMain.style.maxHeight = this._maxHeight + "px";
-    this._divMain.focus();
     this._posX = this._jobber.getIFrame().clientWidth / 2 - this._divMain.clientWidth / 2;
     this._posY =
       this._jobber.getIFrame().clientHeight / 2 - this._divMain.clientHeight / 2;
     this._divMain.style.left = this._posX + "px";
     this._divMain.style.top = this._posY + "px";
+    this.didShow();
   }
 
   public showOnParent(parent: HTMLElement) {
@@ -78,7 +113,7 @@ export class QinJobberPopup {
     this._divMain.style.top = this._posY + "px";
     this._divMain.style.maxWidth = this._maxWidth + "px";
     this._divMain.style.maxHeight = this._maxHeight + "px";
-    this._divMain.focus();
+    this.didShow();
   }
 
   public showOnBounds(bounds: QinBounds) {
@@ -92,13 +127,23 @@ export class QinJobberPopup {
     this._divMain.style.top = this._posY + "px";
     this._divMain.style.maxWidth = this._maxWidth + "px";
     this._divMain.style.maxHeight = this._maxHeight + "px";
+    this.didShow();
+  }
+
+  private didShow() {
     this._divMain.focus();
+    if (this._onShow) {
+      this._onShow.forEach((func) => func());
+    }
   }
 
   public close() {
     if (this._jobber.getIFrameDoc().body.contains(this._divMain)) {
       this._divMain.focus();
       this._jobber.getIFrameDoc().body.removeChild(this._divMain);
+      if (this._onClose) {
+        this._onClose.forEach((func) => func());
+      }
     }
   }
 
